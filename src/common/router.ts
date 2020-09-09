a5f0c4be347754dd5f03649c9919f7c4793fd09c
Kecy Jones <miskecysss@gmail.com>
2020-09-09 02:49:56 -0300

    Set model-router

--- src/common/router.ts
+++ src/common/router.ts
@@ -3,17 +3,30 @@ import { EventEmitter } from 'events'
 import { NotFoundError } from 'restify-errors'
 
 export abstract class Router extends EventEmitter {
-  abstract applyRoutes(application: restify.Server): any
+    abstract applyRoutes(application: restify.Server): any
 
-  render(response: restify.Response, next: restify.Next) {
-    return (document: any) => {
-      if (document) {
-        this.emit('beforeRender', document)
-        response.json(document)
-      } else {
-        throw new NotFoundError('Documento não encontrado')
-      }
-      return next()
+    render(response: restify.Response, next: restify.Next) {
+        return (document: any) => {
+            if (document) {
+                this.emit('beforeRender', document)
+                response.json(document)
+            } else {
+                throw new NotFoundError('Documento não encontrado')
+            }
+            return next()
+        }
     }
-  }
+
+    renderAll(response: restify.Response, next: restify.Next) {
+        return (documents: any[]) => {
+            if (documents) {
+                documents.forEach(document => {
+                    this.emit('beforeRender', document)
+                })
+                response.json(documents)
+            } else {
+                response.json([])
+            }
+        }
+    }
 }
